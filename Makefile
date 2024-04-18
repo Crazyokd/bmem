cd9f1f96f47e440835ff0b19bdf00d8bfa0615a7
Rekord <1324596506@qq.com>
2024-04-18 13:55:00 +0000

    test: add two performance test case

--- Makefile
+++ Makefile
@@ -1,8 +1,11 @@
-all: bmem
+all: bmem test
 CFLAGS=-g -Wall -W -std=c99
 
 bmem: main.c bmem.c
 	$(CC) main.c bmem.c -o bmem $(CFLAGS)
+
+test: perf-test.c bmem.c
+	$(CC) perf-test.c bmem.c -o perf-test $(CFLAGS)
 
 clean:
 	rm -f bmem
---
+++ perf-test.c
@@ -0,0 +1,91 @@
+#include <stdio.h>
+#include <time.h>
+
+#include "bmem.h"
+
+#define ALLOC_CNT 102400000
+
+static void test_interleaved_fashion()
+{
+    clock_t begin, end;
+    double time_spent;
+    int **a = malloc(sizeof(int *) * ALLOC_CNT);
+
+    /* normal malloc and free */
+    begin = clock();
+
+    for (uint32_t i = 0; i < ALLOC_CNT; i++) {
+        a[i] = malloc(sizeof(int));
+        free(a[i]);
+    }
+
+    end = clock();
+
+    time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+    printf("Normal: Time spent: %f seconds\n", time_spent);
+
+    /* Bmem */
+    begin = clock();
+
+    bm_context_t *ctx = bm_context_register(8, 1024, 1024);
+    for (uint32_t i = 0; i < ALLOC_CNT; i++) {
+        a[i] = bm_malloc(ctx, sizeof(int));
+        bm_free_s(ctx, a[i], sizeof(int));
+    }
+    bm_context_destroy(ctx);
+
+    end = clock();
+
+    time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+    printf("Bmem: Time spent: %f seconds\n", time_spent);
+
+    free(a);
+}
+
+static void test_bulk_fashion()
+{
+    clock_t begin, end;
+    double time_spent;
+    int **a = malloc(sizeof(int *) * ALLOC_CNT);
+
+    /* normal malloc and free */
+    begin = clock();
+
+    for (uint32_t i = 0; i < ALLOC_CNT; i++) {
+        a[i] = malloc(sizeof(int));
+    }
+    for (uint32_t i = 0; i < ALLOC_CNT; i++) {
+        free(a[i]);
+    }
+
+    end = clock();
+
+    time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+    printf("Normal: Time spent: %f seconds\n", time_spent);
+
+    /* Bmem */
+    begin = clock();
+
+    bm_context_t *ctx = bm_context_register(8, 1024, 1024);
+    for (uint32_t i = 0; i < ALLOC_CNT; i++) {
+        a[i] = bm_malloc(ctx, sizeof(int));
+    }
+    for (uint32_t i = 0; i < ALLOC_CNT; i++) {
+        bm_free_s(ctx, a[i], sizeof(int));
+    }
+    bm_context_destroy(ctx);
+
+    end = clock();
+
+    time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+    printf("Bmem: Time spent: %f seconds\n", time_spent);
+
+    free(a);
+}
+
+int main()
+{
+    test_interleaved_fashion();
+    test_bulk_fashion();
+    return 0;
+}
